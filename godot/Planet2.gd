0c0087276d59aee6df9a453235bf3901bceffbaa
Eamonn <Eamonn_Ormiston@hotmail.co.uk>
2024-04-13 12:05:28 +0100

    Function to update cells

--- godot/Planet2.gd
+++ godot/Planet2.gd
@@ -1,11 +1,18 @@
 extends Planet
+var m = MeshInstance3D.new()
 var mesh_texture = preload('res://texture.tres')
+
+@export var change_mesh : bool = false
 
 func _ready():
 	draw_mesh(square())
 
 func _process(delta):
-	pass
+	
+	if change_mesh == true:
+		change_mesh = false
+		change_random_cell()
+
 
 func draw_mesh(vertices: PackedVector3Array):
 	var arrays = []
@@ -16,7 +23,6 @@ func draw_mesh(vertices: PackedVector3Array):
 	# Create the Mesh.
 	var arr_mesh = ArrayMesh.new()
 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-	var m = MeshInstance3D.new()
 	m.mesh = arr_mesh
 	m.mesh.surface_set_material(0, mesh_texture)
 	
@@ -46,3 +52,16 @@ func fake_grid_arrays(width: int = 4, height: int = 2) -> Array:
 	output[Mesh.ARRAY_VERTEX] = PackedVector3Array(verticies)
 	output[Mesh.ARRAY_TEX_UV] = PackedVector2Array(UVs)
 	return output
+
+func change_random_cell():
+	change_cell(randi_range(0,7), randi_range(0,10) / 10.0)
+
+func change_cell(cell_number: int, updated_cell_uv: float):
+	var new_arrays = m.mesh.surface_get_arrays(0)
+	for i in range(0,6):
+		new_arrays[Mesh.ARRAY_TEX_UV][cell_number * 6 + i] = Vector2(updated_cell_uv, 0)
+	
+	var arr_mesh = ArrayMesh.new()
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, new_arrays)
+	m.mesh = arr_mesh
+	m.mesh.surface_set_material(0, mesh_texture)
