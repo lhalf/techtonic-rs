1221596f537c357131e303e7f7ce5135b35d21c1
Eamonn <Eamonn_Ormiston@hotmail.co.uk>
2024-04-16 21:07:13 +0100

    Rotating planet removed

--- rust/src/planet.rs
+++ rust/src/planet.rs
@@ -1,30 +1,16 @@
 use godot::engine::{IMeshInstance3D, MeshInstance3D};
-use godot::log::godot_print;
-use godot::obj::{Base, WithBaseField};
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=MeshInstance3D,tool)]
 struct Planet {
-    #[export]
-    angular_speed: f64,
     base: Base<MeshInstance3D>,
 }
 
 #[godot_api]
 impl IMeshInstance3D for Planet {
     fn init(base: Base<MeshInstance3D>) -> Self {
-        godot_print!("Hello, world!");
-
-        Self {
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
-    }
-
-    fn process(&mut self, delta: f64) {
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate_y(radians);
+        Self { base }
     }
 }
 
